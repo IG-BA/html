b4739fd06f0b8dd1f8e5e72c0400e8e3e3e8b658
IG-BA <74450106+IG-BA@users.noreply.github.com>
2021-03-13 11:58:33 +0200

    13.03.school.jquery

---
+++ java script/JS/unit_1/js/index.js
@@ -0,0 +1,31 @@
+
+$(function () {
+    //alert($('h1').text());
+
+    // alert($('.link-moto').text());
+
+    // $('.slide-btn').hide();
+
+    // $('.hd').hide(3000);
+
+    // $('.categorie').show(2000);
+
+    // alert($('li').text());
+
+    // $('.swhd').hide(3500);
+    // $('.swhd').show(2000);
+
+    // $('h3 > span').hide(1000);
+    // $('h3 > span').show(2000);
+
+    // alert($('.sp').text());
+
+    // alert($('.nm').text());
+
+    alert($('.post-head').text());
+})
+
+
+
+
+
