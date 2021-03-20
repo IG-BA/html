336ba1d5a87df397e1423d37afa94b933398c83a
IG-BA <74450106+IG-BA@users.noreply.github.com>
2021-03-20 12:00:20 +0200

    20.03.distance

---
+++ java script/JS/unit_1/js/fnct.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    hide("my_form");
+
+    function hide(name_id_obj){
+        obj=$("#"+name_id_obj);
+    }
+
+
+
+
+})
--- java script/JS/unit_1/js/index.js
+++ java script/JS/unit_1/js/index.js
@@ -1,6 +1,6 @@
 
 $(function () {
-    //alert($('h1').text());
+    //alert($('h1').text());  //t1
 
     // alert($('.link-moto').text());
 
@@ -22,7 +22,79 @@ $(function () {
 
     // alert($('.nm').text());
 
-    alert($('.post-head').text());
+    // alert($('.post-head').text());
+
+    // $('.i').hide();
+
+    // hide("my_form");  //t2 
+    // hide("img_2");
+    // // hide("moto_models");
+
+    // function hide(name_id_obj){
+    //     obj=$("#"+name_id_obj);
+    //     obj.hide(3000);
+    //     obj.show(3000);
+    // }
+
+    // $('h1').fadeOut(3000);
+    // $('h1').fadeIn(3000);
+
+    // $(".head-moto-img").fadeTo(7000, 0.2)
+    // $(".head-moto-img").fadeTo(7000, 0.8)
+
+    // $("h1").slideUp(3000)
+    // $("h1").slideDown(3000)
+
+    // $(".head-moto-img").fadeOut(3000, function(){
+    //     alert('ended')
+    // });
+
+    // $('.blog-post').fadeOut(1500);
+    // $(".blog-time-line").slideUp(2000);
+    // $(".blog-time-line").slideDown(3000);
+    // $(".blog-post").fadeIn(6000);
+
+    // zr("#img_1", 2000, 10000);
+    // zr("#img_2", 2000, 10000);
+    // zr("#img_3", 2000, 10000);
+    // zr("#img_4", 2000, 10000);
+
+    // function zr(name_obj, t1, t2){
+    //     obj = $("name_obj");
+    //     obj.slideUp(t1);
+    //     obj.slideDown(t2);
+
+    // }
+    // trnsp("#img_1", 2000, 0.3);
+    // trnsp("#img_2", 2000, 0,7);
+    // trnsp("#img_3", 2000, 0.3);
+    // trnsp("#img_4", 2000, 0);
+    
+    // function trnsp(name_obj, time, opacity){
+    //     obj = $(name_obj);
+    //     obj.fadeTo(time, opacity);
+    //     obj.slideUp(time);
+    //     obj.slideDown(time);
+    //     obj.fadeTo(time, 1);
+
+    // }
+
+    
+    // slide("#img_1", 3000, 2000);
+    // slide("#img_2", 2000, 3000);
+    // slide("#img_3", 3000, 2000);
+    // slide("#img_4", 2000, 3000);
+    
+    // function slide(name_obj, time1, time2){
+    //     obj = $(name_obj);
+    //     obj.slideUp(time1);
+    //     obj.slideDown(time2);
+        
+
+    // }
+
+
+
 })
 
 
